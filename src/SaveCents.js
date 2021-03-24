6c1b62f1f77b46533d56b9a7c639624e551e4e4a
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 16:30:38 -0400

    Work on SaveCent box

--- src/SaveCents.js
+++ src/SaveCents.js
@@ -2,10 +2,27 @@ import React from "react";
 import "./SaveCents.css";
 
 const SaveCents = () => {
+	let wordGreet = "Hey";
+	let name = "Sarah";
+	// let supportMessage =
+	const words = `${wordGreet} ${name}!`;
+
 	return (
 		<div className="save-cents-picker">
-			<div className="save-cents-picker-top"></div>
-			<div className="save-cents-picker-middle"></div>
+			<div className="save-cents-picker-top">
+				<p className="x-close">X</p>
+			</div>
+			<div className="save-cents-picker-middle">
+				<p>{words}</p>
+				<hr></hr>
+				<h5>Support Small Businesses!</h5>
+				<p>
+					Our mission is to help local lodging get back on their feet while
+					helping travelers productively save for their trips
+				</p>
+				{/* {supportMessage} */}
+			</div>
+			<div className="save-cents-picker-bottom"></div>
 		</div>
 	);
 };
