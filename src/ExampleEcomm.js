5e0219fbd85f38031ed31e539bb1890104675dab
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 15:59:45 -0400

    Add SaveCents component

--- src/ExampleEcomm.js
+++ src/ExampleEcomm.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./ExampleEcomm.css";
+import SaveCents from "./SaveCents";
 
 const ExampleEcomm = () => {
 	return (
-		<div className="exampleSite">
+		<div className="example-site">
 			<h2>e-Commerce Site</h2>
 			<h3>Checkout Page</h3>
-			<div className="orderSummary">
+			<SaveCents />
+			<div className="order-summary">
 				<h4>Order Summary</h4>
 				<ul>
 					<li>Dog Collar $14.99</li>
