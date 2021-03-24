a73319b4eef0594fac42fc286d98a7572fd82ddf
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 19:58:01 -0400

    Try logo ideas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 import "./App.css";
 import ExampleEcomm from "./ExampleEcomm";
+import logo from "./travel-cents.png";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				{/* <img src={logo} className="App-logo" alt="logo" /> */}
-				<h1>Travel Cents</h1>
+				<h1 className="header-h1">Travel Cents</h1>
+				{/* <img src={logo} className="travel-cents-logo" alt="logo" /> */}
 			</header>
 			<ExampleEcomm />
 		</div>
