8f0cb9c4fb704424164268762486dcd7943bcb58
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 16:00:21 -0400

    Add SaveCent and style basic

---
+++ src/SaveCents.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SaveCents.css";
+
+const SaveCents = () => {
+	return (
+		<div className="save-cents-picker">
+			<div className="save-cents-picker-top"></div>
+			<div className="save-cents-picker-middle"></div>
+		</div>
+	);
+};
+
+export default SaveCents;
