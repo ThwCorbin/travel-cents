a7740d8499daa7e2254d277dec2cf5fccb8822a8
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 22:49:51 -0400

    Destructor props

--- src/Button.js
+++ src/Button.js
@@ -1,25 +1,25 @@
 import "./Button.css";
 
-const Button = (props) => {
+const Button = ({ large, small, outline, label }) => {
 	// Only using types "primary"
 	let classList = "button";
 
 	// Check if large, small, and/or outline props are true
 	// Update classList variable
 	// must keep " " at String start to build "button button-primary" + " button-large"
-	if (props.large) {
+	if (large) {
 		classList += ` button-large`;
 	}
 
-	if (props.small) {
+	if (small) {
 		classList += ` button-small`;
 	}
 
-	if (props.outline) {
+	if (outline) {
 		classList += ` button-outline`;
 	}
 
-	return <button className={classList}>{props.label}</button>;
+	return <button className={classList}>{label}</button>;
 };
 
 export default Button;
