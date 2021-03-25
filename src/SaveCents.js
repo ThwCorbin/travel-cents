249d4dbd95c19563c148dbafbf944d4cc753fb28
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 20:30:42 -0400

    Style SaveCents

--- src/SaveCents.js
+++ src/SaveCents.js
@@ -7,28 +7,28 @@ const SaveCents = () => {
 	let wordSay = "Hey";
 	let name = "Sarah";
 	// let supportMessage =
-	const wordsMiddle = `${wordSay} ${name}!`;
+	const wordsForUser = `${wordSay} ${name}!`;
 	let cents = "0.42";
 	let wordsFund = `Do you want to round up on this and put $${cents} towards your travel funds?`;
 
 	return (
 		<div className="save-cents-picker">
 			<div className="save-cents-picker-top">
-				<p className="x-close">X</p>
-				<img src={houseLogo} alt="Travel Cent House" />
+				<p className="p-x-close">X</p>
+				<img src={houseLogo} className="house-logo" alt="House Logo" />
 			</div>
 			<div className="save-cents-picker-middle">
-				<p>{wordsMiddle}</p>
+				<p className="p-words-for-user">{wordsForUser}</p>
 				<hr></hr>
-				<h5 className="h5-support">Support Small Businesses!</h5>
-				<p className="mission-statement">
+				<h5 className="h5-support-mission">Support Small Businesses!</h5>
+				<p className="p-support-mission">
 					Our mission is to help local lodging get back on their feet while
 					helping travelers productively save for their trips
 				</p>
 				{/* {supportMessage} */}
 			</div>
 			<div className="save-cents-picker-bottom">
-				<p className="words-fund">{wordsFund}</p>
+				<p className="p-words-fund">{wordsFund}</p>
 				<div className="button-choices">
 					<Button outline label="$0.42" />
 					<Button outline label="$1.00" />
