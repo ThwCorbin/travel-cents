a3d1d7c07c3d37f0c50caf9ce88a6ac183390ec5
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 22:24:45 -0400

    Make <header> a Header component, delete test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-// import logo from './logo.svg';
 import "./App.css";
 import ExampleEcomm from "./ExampleEcomm";
-import logo from "./travel-cents.png";
+import Header from "./Header";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1 className="header-h1">Travel Cents</h1>
-				{/* <img src={logo} className="travel-cents-logo" alt="logo" /> */}
-			</header>
+			<Header />
 			<ExampleEcomm />
 		</div>
 	);
