2f89713c6d62f87edb6075874646219ab452ccf5
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 22:22:00 -0400

    Make <header> a Header component

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+// import logo from "./travel-cents.png";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<h1 className="header-h1">Travel Cents</h1>
+			{/* <img src={logo} className="travel-cents-logo" alt="logo" /> */}
+		</header>
+	);
+};
+
+export default Header;
