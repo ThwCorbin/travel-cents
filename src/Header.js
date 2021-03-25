293665e4c8fe92dca74bafa8667227ba7016b860
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-03-24 23:19:47 -0400

    Work Header options

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,29 @@
 import "./Header.css";
-// import logo from "./travel-cents.png";
+import logoHouse from "./travel-cents-house.png";
 
-const Header = () => {
-	return (
-		<header className="header">
-			<h1 className="header-h1">Travel Cents</h1>
-			{/* <img src={logo} className="travel-cents-logo" alt="logo" /> */}
-		</header>
-	);
+const Header = ({ exampleECommSite }) => {
+	if (exampleECommSite) {
+		return (
+			<header className="header-e-comm-site">
+				<h1 className="header-h1">Travel Cents</h1>
+				<h2 className="header-h2">e-Commerce Site Example</h2>
+				{/* <img src={logo} className="travel-cents-logo" alt="logo" /> */}
+			</header>
+		);
+	} else {
+		return (
+			<header className="header-travel-cents">
+				<img src={logoHouse} className="header-logo-house" alt="House Logo" />
+				<nav>
+					<ul className="ul-nav">
+						<li key="find-lodging">Find Lodging</li>
+						<li key="become-partner">Become a Partner</li>
+						<li key="account">Account</li>
+					</ul>
+				</nav>
+			</header>
+		);
+	}
 };
 
 export default Header;
