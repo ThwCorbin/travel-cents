52a4b58eed12951bed0941ad1e6caa544bba7f19
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-04-06 10:31:57 -0400

    Convert App into a Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-import ExampleEcomm from "./components/ExampleEcomm";
-import Account from "./components/Account";
-import Header from "./components/Header";
+// import ExampleEcomm from "./components/ExampleEcomm";
+// import Account from "./components/Account";
+// import Header from "./components/Header";
 
 function App() {
 	return (
-		<div className="App">
-			<ExampleEcomm />
-			{/* <Account /> */}
-		</div>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/lodging">Find Lodging</Link>
+						</li>
+						<li>
+							<Link to="/partner">Become a Partner</Link>
+						</li>
+						<li>
+							<Link to="/account">My Account</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+
+			<Switch>
+				<Route path="/lodging">
+					<Lodging />
+				</Route>
+				<Route path="/partner">
+					<Partner />
+				</Route>
+				<Route path="/account">
+					<Account />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</Router>
 	);
+}
+
+function Home() {
+	return <h2>Home</h2>;
+}
+
+function Lodging() {
+	return <h2>Find Lodging</h2>;
+}
+
+function Partner() {
+	return <h2>Become a Partner</h2>;
+}
+
+function Account() {
+	return <h2>My Account</h2>;
 }
 
 export default App;
