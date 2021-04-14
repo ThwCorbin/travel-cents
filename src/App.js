2a90c04d44e15eb14e5f3a16c96b099baa9e5060
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-04-14 17:39:05 -0400

    Delete App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
 import Home from "./components/Home";
 import Demo from "./components/Demo";
 import SignUp from "./components/SignUp";
